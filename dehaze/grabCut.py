5af43942e408df92191952edd5c4dc93c8ff18a2
Yunseo Kim <kys051348@gmail.com>
2022-07-24 04:10:23 +0900

    update

--- dehaze/grabCut.py
+++ dehaze/grabCut.py
@@ -1,14 +1,29 @@
 import cv2
+import sys
 import numpy as np
 
-# https://deep-learning-study.tistory.com/240 
+# https://deep-learning-study.tistory.com/240
 
 if __name__ == "__main__":
-    I = cv2.imread('data/haze_image.png')
-    # hsv = cv2.cvtColor(I, cv2.COLOR_BGR2HSV)
-    # h, s, v = cv2.split(hsv)
-    # print(v)
-    # print(type(v))
-    # print(np.shape(v))
-    cv2.imshow('test',I)
+    src = cv2.imread('data/groot.jpg')
+    if src is None:
+        print('Image load failed!')
+        sys.exit()
+    rc = cv2.selectROI(src)  # 초기 위치 지정하고 모서리 좌표 4개를 튜플값으로 반환
+    mask = np.zeros(src.shape[:2], np.uint16)  # 마스크는 검정색으로 채워져있고 입력 영상과 동일한 크기
+
+    # 결과를 계속 업데이트 하고 싶으면 bgd, fgd 입력
+    cv2.grabCut(src, mask, rc, None, None, 5, cv2.GC_INIT_WITH_RECT)
+
+    # grabCut 자료에서 0,2는 배경, 1,3은 전경입니다.
+    # mask == 0 or mask == 2를 만족하면 0으로 설정 아니면 1로 설정합니다
+    mask2 = np.where((mask == 0) | (mask == 2), 0, 1).astype('uint16')
+
+    # np.newaxis로 차원 확장
+    dst = src * mask2[:, :, np.newaxis]
+    
+    mask = mask * 64
+    cv2.imshow('mask', mask)
+    cv2.imshow('dst', dst)
     cv2.waitKey()
+    cv2.destroyAllWindows()
--- dehaze/test.py
+++ dehaze/test.py
@@ -7,22 +7,35 @@ import matplotlib.pyplot as plt
 BINS = 256
 MAX_LEVEL = BINS - 1
 
+
 def i2f(img):
     return img / MAX_LEVEL
+
 
 def f2i(img):
     return np.uint8(np.around(img * MAX_LEVEL))
 
-# def segmentation(img):
-#     original = 
+
+def transform(img):
+    h, w = img.shape[:2]
+    new_image = np.zeros(img.shape, img.dtype)
+    for i in range(h):
+        for j in range(w):
+            if(img[i][j] > 125):  # 우선 명도의 절반만큼
+                new_image[i][j] = img[i][j]
+    cv2.imshow('test2', new_image)
+    cv2.imwrite('dehaze/outputs/reuslt.jpg', new_image)
+    return new_image
+
 
 if __name__ == "__main__":
-    I = cv2.imread('dehaze/haze_depth.jpg')
+    # DCP 방법처럼 갈게 일단은 -> 우선 rgb를 split하고, 걔네들의 명도를 각각 따로 구하고 transform 처리한 후 return 할 때 다 더해주면 됨
+    I = cv2.imread('data/haze_depth.jpg')
     hsv = cv2.cvtColor(I, cv2.COLOR_BGR2HSV)
     h, s, v = cv2.split(hsv)
-    print(v)
-    print(type(v))
-    print(np.shape(v))
-    cv2.imshow('test',v)
+    test_result = transform(v)
+    result = cv2.merge([h, s, test_result])
+    cv2.imshow('test', v)
+    cv2.imshow('rseult', result)
+    cv2.imwrite('dehaze/outputs/value of depth.jpg', v)
     cv2.waitKey()
-    
