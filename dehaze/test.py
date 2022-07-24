4552740267f3c1a0e367c3056148ec2defb17348
Yunseo <kys051348@gmail.com>
2022-07-24 13:12:49 +0900

    near&far

--- dehaze/test.py
+++ dehaze/test.py
@@ -18,24 +18,34 @@ def f2i(img):
 
 def transform(img):
     h, w = img.shape[:2]
-    new_image = np.zeros(img.shape, img.dtype)
+    near_image = np.zeros(img.shape, img.dtype)
+    far_image = np.zeros(img.shape, img.dtype)
     for i in range(h):
         for j in range(w):
-            if(img[i][j] > 125):  # 우선 명도의 절반만큼
-                new_image[i][j] = img[i][j]
-    cv2.imshow('test2', new_image)
-    cv2.imwrite('dehaze/outputs/reuslt.jpg', new_image)
-    return new_image
+            if(img[i][j] > BINS / 2):  # 우선 명도의 절반만큼
+                near_image[i][j] = img[i][j]
+            else:
+                far_image[i][j] = img[i][j]
+    cv2.imshow('near', near_image)
+    cv2.imshow('far', far_image)
+    cv2.imwrite('dehaze/outputs/near image.jpg', near_image)
+    cv2.imwrite('dehaze/outputs/far image.jpg', far_image)
+    return near_image, far_image
 
 
 if __name__ == "__main__":
     # DCP 방법처럼 갈게 일단은 -> 우선 rgb를 split하고, 걔네들의 명도를 각각 따로 구하고 transform 처리한 후 return 할 때 다 더해주면 됨
-    I = cv2.imread('data/haze_depth.jpg')
-    hsv = cv2.cvtColor(I, cv2.COLOR_BGR2HSV)
-    h, s, v = cv2.split(hsv)
-    test_result = transform(v)
-    result = cv2.merge([h, s, test_result])
+    depth_I = cv2.imread('data/haze_depth.jpg')
+    # hsv = cv2.cvtColor(I, cv2.COLOR_BGR2HSV)
+    # h, s, v = cv2.split(hsv)
+    # cvtColor(I, cv2.COLOR_BGR2GRAY)
+    for i in range(3):
+
+
+    near_result, far_result = transform(v)
+    # result = cv2.merge([h, s, near_result])
+    # result = cv2.merge([h, s, far_result])
     cv2.imshow('test', v)
     cv2.imshow('rseult', result)
     cv2.imwrite('dehaze/outputs/value of depth.jpg', v)
-    cv2.waitKey()
+    cv2.waitKey(0)
