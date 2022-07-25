5739baa532a383211e0f968a5a54633aaec45bde
Yunseo Kim <kys051348@gmail.com>
2022-07-26 02:24:12 +0900

    go

--- dehaze/test.py
+++ dehaze/test.py
@@ -15,37 +15,42 @@ def i2f(img):
 def f2i(img):
     return np.uint8(np.around(img * MAX_LEVEL))
 
+def get_value(img):
+    return np.average(img, 2)
 
-def transform(img):
+
+def transform(img, I): # img: depth, I: Hazy image
     h, w = img.shape[:2]
     near_image = np.zeros(img.shape, img.dtype)
     far_image = np.zeros(img.shape, img.dtype)
     for i in range(h):
         for j in range(w):
-            if(img[i][j] > BINS / 2):  # 우선 명도의 절반만큼
-                near_image[i][j] = img[i][j]
+            if(img[i][j] >= 110): 
+                near_image[i][j] = img[i][j] 
             else:
-                far_image[i][j] = img[i][j]
+                far_image[i][j] = img[i][j] 
+    # 각각 I랑 합성시키고, near_image에서는 I의 배경이 들어가고
+    # far_image에는 I의 사람이 들어감
+    # I랑 합성한 near_image를 SBTE 알고리즘 돌려
+    # 그러면 지금 나온 게 far_image의 정상인 사람과 안개제거된 배경이 있음
+    # 나온 결과 두 개를 합성함.             
     cv2.imshow('near', near_image)
     cv2.imshow('far', far_image)
-    cv2.imwrite('dehaze/outputs/near image.jpg', near_image)
-    cv2.imwrite('dehaze/outputs/far image.jpg', far_image)
+    cv2.imwrite('dehaze/outputs/near_image.jpg', near_image)
+    cv2.imwrite('dehaze/outputs/far_image.jpg', far_image)
     return near_image, far_image
 
 
 if __name__ == "__main__":
     # DCP 방법처럼 갈게 일단은 -> 우선 rgb를 split하고, 걔네들의 명도를 각각 따로 구하고 transform 처리한 후 return 할 때 다 더해주면 됨
+    I = cv2.imread('data/haze_image.jpg')
     depth_I = cv2.imread('data/haze_depth.jpg')
-    # hsv = cv2.cvtColor(I, cv2.COLOR_BGR2HSV)
-    # h, s, v = cv2.split(hsv)
-    # cvtColor(I, cv2.COLOR_BGR2GRAY)
-    for i in range(3):
-
-
-    near_result, far_result = transform(v)
-    # result = cv2.merge([h, s, near_result])
-    # result = cv2.merge([h, s, far_result])
-    cv2.imshow('test', v)
-    cv2.imshow('rseult', result)
-    cv2.imwrite('dehaze/outputs/value of depth.jpg', v)
-    cv2.waitKey(0)
+    v = get_value(depth_I)
+    a, b = transform(v, I)
+    # result1 = cv2.merge([h, s, a])
+    # result2 = cv2.merge([h, s, b])
+    # cv2.imshow('test', v)
+    # cv2.imshow('rseult', result1)
+    # cv2.imshow('rseult', result2)
+    # cv2.imwrite('dehaze/outputs/value of depth.jpg', v)
+    cv2.waitKey()
