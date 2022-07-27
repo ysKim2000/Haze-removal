06e9125bdbc602d4d9e3454551d90c0b9766732d
Yunseo Kim <kys051348@gmail.com>
2022-07-27 18:40:38 +0900

    .

--- dehaze/test.py
+++ dehaze/test.py
@@ -1,4 +1,5 @@
 import cv2
+from cv2 import imwrite
 import numpy as np
 import os
 from skimage import morphology
@@ -19,13 +20,13 @@ def get_value(img):
     return np.average(img, 2)
 
 
-def transform(img, I): # img: depth, I: Hazy image
+def transform(img, I, sbte): # img: depth, I: Hazy image, sbte: dehazy image
     h, w = img.shape[:2]
     near_image = np.zeros(img.shape, img.dtype)
     far_image = np.zeros(img.shape, img.dtype)
     for i in range(h):
         for j in range(w):
-            if(img[i][j] >= 110): 
+            if(img[i][j] >= 85): 
                 near_image[i][j] = img[i][j] 
             else:
                 far_image[i][j] = img[i][j] 
@@ -33,7 +34,14 @@ def transform(img, I): # img: depth, I: Hazy image
     # far_image에는 I의 사람이 들어감
     # I랑 합성한 near_image를 SBTE 알고리즘 돌려
     # 그러면 지금 나온 게 far_image의 정상인 사람과 안개제거된 배경이 있음
-    # 나온 결과 두 개를 합성함.             
+    # 나온 결과 두 개를 합성함.     
+    print(np.shape(I))
+    print(np.shape(near_image))
+    print(np.shape(sbte))
+    masked = cv2.copyTo(I, near_image, sbte)
+    cv2.imshow('masked', masked)
+    cv2.imwrite('dehaze/outputs/result.jpg', masked)
+    
     cv2.imshow('near', near_image)
     cv2.imshow('far', far_image)
     cv2.imwrite('dehaze/outputs/near_image.jpg', near_image)
@@ -42,15 +50,11 @@ def transform(img, I): # img: depth, I: Hazy image
 
 
 if __name__ == "__main__":
-    # DCP 방법처럼 갈게 일단은 -> 우선 rgb를 split하고, 걔네들의 명도를 각각 따로 구하고 transform 처리한 후 return 할 때 다 더해주면 됨
     I = cv2.imread('data/haze_image.jpg')
     depth_I = cv2.imread('data/haze_depth.jpg')
-    v = get_value(depth_I)
-    a, b = transform(v, I)
-    # result1 = cv2.merge([h, s, a])
-    # result2 = cv2.merge([h, s, b])
-    # cv2.imshow('test', v)
-    # cv2.imshow('rseult', result1)
-    # cv2.imshow('rseult', result2)
-    # cv2.imwrite('dehaze/outputs/value of depth.jpg', v)
+    sbte = cv2.imread('data/sbte.jpg')
+    # v = get_value(depth_I)
+    v = cv2.cvtColor(depth_I, cv2.COLOR_BGR2GRAY)
+    a, b = transform(v, I, sbte)
+
     cv2.waitKey()
